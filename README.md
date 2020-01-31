9e7603e84ef9dbf0b777af7ee8e56803500bef5e
Zhongxian Chen <dandanyueguang@gmail.com>
2020-01-31 10:46:40 +0800

    0.1.0版本发布

---
+++ README.md
@@ -0,0 +1,89 @@
+# HCHO 甲醛传感器 Mind+ 用户库
+
+![](./arduinoC/_images/featured.png)
+
+---------------------------------------------------------
+
+## 链接
+
+- **本用户库加载地址：**[https://gitee.com/ironpanda/ext-DFRobotHCHOSensor](https://gitee.com/ironpanda/ext-DFRobotHCHOSensor)
+- **Mind+下载地址：**[http://mindplus.cc](http://mindplus.cc)
+
+## 简介
+
+- **产品链接：**[https://www.dfrobot.com.cn/goods-1373.html](https://www.dfrobot.com.cn/goods-1373.html)
+- **介绍：**本扩展库为 DFRobot HCHO甲醛传感器（SEN0231）设计，支持 Mind+ 导入库
+
+## 导入教程
+
+在 Mind+ 中选择用户库，然后输入网址或者点击“导入文件”即可。
+
+![import](arduinoC/_images/import.png)
+
+以导入文件方式为例，点击“导入文件”，在弹出的资源管理器中，定位到库文件路径，然后选择config.json文件，即可完成导入。
+
+![import2](arduinoC/_images/import2.png)
+
+导入成功后，就会出现相应的传感器库可供选择
+
+![import3](arduinoC/_images/import3.png)
+
+点击相应的用户库进行选择，就会出现在“已加载”分类中，与自带库的操作一致。
+
+![import4](arduinoC/_images/import4.png)
+
+回到 Mind+ 主界面，这时就可以在“用户库”分类中看到刚才选择的传感器的对应图形模块。
+
+![import5](arduinoC/_images/import5.png)
+
+## Blocks
+
+![](./arduinoC/_images/block.png)
+
+## 示例
+
+本示例将演示如何使用这款甲醛传感器来测量空气中甲醛的浓度值。
+接线方式分两种，分别是 UART 模式与 DAC 模式。
+
+### UART 模式
+
+使用UART模式前，请先将拨码开关切换到 UART 一端。
+
+接线图：
+
+![wire_UART](./arduinoC/_images/wire_UART.png)
+
+Mind+示例程序：
+
+![](./arduinoC/_images/example1.png)
+
+### DAC 模式
+
+使用 DAC 模式前，请先将拨码开关切换到 DAC 一端。
+
+> DAC模式中，测量精度会受主控器ADC的位数、参考电压精度的影响，因此请使用高精度的电源给主控器供电，或者直接使用主控器的内部参考电压。而且主控器的ADC至少是10位。
+
+接线图：
+
+![wire_DAC](./arduinoC/_images/wire_DAC.png)
+
+Mind+示例程序：
+
+![](./arduinoC/_images/example3.png)
+
+## License
+
+MIT
+
+## 硬件支持
+
+MCU                | JavaScript    | Arduino   | MicroPython    | Remarks
+------------------ | :----------: | :----------: | :---------: | -----
+micro:bit        |             |       √       |             | 
+mPython（掌控版）        |             |        √      |             | 
+Arduino Uno    |             |        √      |             | 
+
+## 更新日志
+
+- V0.1.0 20200131 第一版发布
+
---
+++ arduinoC/main.ts
@@ -0,0 +1,71 @@
+
+//% color="#2F6869" iconWidth=40 iconHeight=40
+namespace HCHOSensor {
+    //% block="HCHO sensor initliallize pin [SSER] RX [SSTXD] TX [SSRXD]" blockType="command"
+    //% SSER.shadow="dropdown" SSER.options="SSER"
+    //% SSTXD.shadow="dropdown" SSTXD.options="SSTXD"
+    //% SSRXD.shadow="dropdown" SSRXD.options="SSRXD"
+    export function beginSoftSerial(parameter: any, block: any) {
+        let sser = "hcho" + parameter.SSER.code;
+        let tx = parameter.SSTXD.code;
+        let rx = parameter.SSRXD.code;
+
+        Generator.addInclude("includeHCHO", "#include <DFRobotHCHOSensor.h>");
+        Generator.addInclude("includesoftSerial", "#include <SoftwareSerial.h>");
+        // Generator.addObject("quanjubianliang", "float", `GTVar=-1;`);
+        Generator.addObject("softSerialObject", "SoftwareSerial", `${sser}(${tx}, ${tx});`);
+        Generator.addObject("HCHOObject" + sser, "DFRobotHCHOSensor", `hchoSensor(&${sser});`);
+        
+        Generator.addSetup("HCHOSerialSetup" + sser, `${sser}.begin(9600);`);
+        Generator.addSetup("HCHOSerialListen" + sser, `${sser}.listen();`);
+    }
+
+    //% block="HCHO sensor initliallize pin [SER] RX [TXD] TX [RXD]" blockType="command"
+    //% SER.shadow="dropdown" SER.options="SER"
+    //% RXD.shadow="dropdown" RXD.options="RXD"
+    //% TXD.shadow="dropdown" TXD.options="TXD"
+    export function beginSerial(parameter: any, block: any) {
+        let ser = parameter.SER.code;
+        let rx = parameter.RXD.code;
+        let tx = parameter.TXD.code;
+        Generator.addInclude("includeHCHO", "#include <DFRobotHCHOSensor.h>");
+        Generator.addObject("HCHOObject" + ser, "DFRobotHCHOSensor", `hchoSensor(&${ser});`);
+        if (Generator.board === 'arduino') {
+            Generator.addSetup("HCHOSerialSetup", `${ser}.begin(9600);`);
+        } else if (Generator.board === 'esp32') {
+            Generator.addSetup("HCHOSerialSetup", `${ser}.begin(9600,${rx}, ${tx});`);
+        }
+        // Generator.addSetup("HCHOSerialListen" + ser, `${ser}.listen();`);
+    }
+
+    //% block="HCHO sensor available" blockType="boolean"
+    export function available() {
+        Generator.addCode("hchoSensor.available() > 0");
+    }
+
+    //% block="Read HCHO sensor value (UART Mode)" blockType="reporter"
+    export function uartReadPPM() {
+        Generator.addCode("hchoSensor.uartReadPPM()");
+    }
+
+    //% block="Read HCHO sensor value pin [APIN] (DAC Mode)" blockType="reporter"
+    //% APIN.shadow="dropdown" APIN.options="APIN"
+    export function dacReadPPM(parameter: any, block: any) {
+        let ax = parameter.APIN.code;
+        Generator.addInclude("includeHCHO", "#include <DFRobotHCHOSensor.h>");
+        if (Generator.board === 'arduino') {
+            Generator.addObject("HCHOObject" + ax, "DFRobotHCHOSensor", `hchoSensor(${ax}, 5.0);`);
+        // } else if (Generator.board === 'arduinounor3') {
+        //     Generator.addObject("HCHOObject" + ax, "DFRobotHCHOSensor", `hchoSensor(${ax}, 5.0);`);
+        // } else if (Generator.board === 'arduinonano') {
+        //     Generator.addObject("HCHOObject" + ax, "DFRobotHCHOSensor", `hchoSensor(${ax}, 5.0);`);
+        // } else if (Generator.board === 'leonardo') {
+        //     Generator.addObject("HCHOObject" + ax, "DFRobotHCHOSensor", `hchoSensor(${ax}, 5.0);`);
+        // } else if (Generator.board === 'mega2560') {
+        //     Generator.addObject("HCHOObject" + ax, "DFRobotHCHOSensor", `hchoSensor(${ax}, 5.0);`);
+        } else {
+            Generator.addObject("HCHOObject" + ax, "DFRobotHCHOSensor", `hchoSensor(${ax}, 3.3);`);
+        }
+        Generator.addCode("hchoSensor.dacReadPPM()");
+    }
+}
